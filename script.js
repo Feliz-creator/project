d9b9c6f55b6ca5277580ccfd0810469cc2a307b7
Natasha <example@gmail.com>
2020-05-09 15:15:15 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict' ;
+
+console.log(leftBorderWidth);
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+
+let arr = ['plum.png','orange.jpg', 'apple.bmp'];
+
+console.log(arr[2]);
+
+/*alert('Hello bitch!');
+console.log(answer);
+
+let answer = prompt('Есть ли вам 18?', 'да');
+
+console.log(answer);*/
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
